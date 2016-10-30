bf3f13f1fc8859427184ef15a7b946aa5e99ea0f
antharamshivakumar <antharamshivakumar@gmail.com>
2016-10-30 22:07:21 +0530

    Add files via upload

---
+++ assignment 5.3.js
@@ -0,0 +1,15 @@
+var employee = {
+    
+    "name":"shiva",
+    "age": 22,
+    "salary": 30000,
+    "address":{
+        
+        "pin" : 12345,
+        "city" : "xyz",
+        "state" : "telangana"
+    }
+};
+
+
+console.log(employee);
